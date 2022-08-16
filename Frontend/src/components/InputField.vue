6479e084767d5f994645c96b4ffd4b3c3d4f915d
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-08-16 21:12:55 +0530

    Frontend

---
+++ Frontend/src/components/InputField.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="postData" method="POST">
+    <!--Setting up a on click function to post data-->
+    <div>
+      <label for="User ">Enter Date</label>
+      <input v-model="date" required placeholder="YYYY-MM-DD" />
+      
+    </div>
+
+    <button @click="update">Submit</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios"; //Importing axios
+
+export default {
+  name: "InputField",
+
+  data() {
+    return {
+      //Setting data schema
+      date: "",
+    };
+  },
+
+  methods: {
+    postData() {
+
+      let data = { date:  this.date }
+      console.log(data)
+
+      axios
+        .post("http://localhost:3000/getting-data", data ) // port('THE URL LINK FOR POST API', THE DATA TO BE SENT)
+        .then((response) => console.log(response))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,80 @@
+<template>
+
+<div class="head">
+
+<h1>Hello World</h1>
+<p></p>
+</div>
+
+
+
+<div class="dataDrive">  
+<InputField />
+</div>
+
+
+<img v-bind:src="date" />
+
+
+
+
+
+</template>
+
+<script>
+import InputField from '../components/InputField.vue'
+import axios from 'axios';
+
+export default {
+  name: "Home",
+  components:{
+    InputField,
+  },
+  
+  data(){
+    return{
+      date:''   //Data schema to store data locally for rendering.
+    }
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:3000/date")   //get('URL FROM WHERE WE WILL RECIEVE DATA')
+      .then((response) => {
+        this.date = response.data   // Setting the response data as data.
+        console.log(this.date);
+      }).catch(err => {console.log(err)
+        this.errored=true})
+        
+  },
+};
+</script>
+
+
+<style scoped>
+
+.head{
+    display: grid;
+    align-content: center;
+    justify-content: center;
+    width:100vw;
+    height:60vh;
+    padding:20px;
+    font-size: 2rem;
+}
+
+
+.dataDrive{
+    text-align: left;
+    margin: 5vh auto;
+    border-radius: 20px;
+    background-color: aliceblue;
+    width:80vw;
+    padding: 10px;
+    
+}
+img{
+
+}
+
+</style>
